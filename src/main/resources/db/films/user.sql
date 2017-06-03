22f9aa5815b196766906f641d6a7467f8fa2faa7
dzinushi <dzinushi.kun@gmail.com>
2017-06-03 15:40:11 +0300

    remove some unusable columns

--- src/main/resources/db/films/user.sql
+++ src/main/resources/db/films/user.sql
@@ -1,8 +1,5 @@
 CREATE TABLE user (
   id BIGINT PRIMARY KEY AUTO_INCREMENT,
   login VARCHAR(16) UNIQUE,
-  password VARCHAR(64),
-  created_at timestamp,
-  is_deleted boolean,
-  deleted_at timestamp
+  password VARCHAR(64)
 );
