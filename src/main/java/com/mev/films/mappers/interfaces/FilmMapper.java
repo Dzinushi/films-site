4f6e251046acaece1d5ecc2f5f615aa51038547a
dzinushi <dzinushi.kun@gmail.com>
2017-06-06 18:21:19 +0300

    Added FilmMapper

---
+++ src/main/java/com/mev/films/mappers/interfaces/FilmMapper.java
@@ -0,0 +1,12 @@
+package com.mev.films.mappers.interfaces;
+
+import com.mev.films.model.FilmDTO;
+
+import java.util.List;
+
+public interface FilmMapper {
+    List<FilmMapper> selectFilms();
+    Long insertFilm(FilmDTO filmDTO);
+    Long updateFilm(FilmDTO filmDTO);
+    Long removeFilmById(Long id);
+}
