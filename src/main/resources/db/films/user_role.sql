a03e7dbc05e654325616e62743ddb5b60b7d642d
dzinushi <dzinushi.kun@gmail.com>
2017-06-09 15:21:00 +0300

    Create user_role table

---
+++ src/main/resources/db/films/user_role.sql
@@ -0,0 +1,9 @@
+CREATE SEQUENCE user_roles_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+CREATE TABLE user_roles (
+  id    BIGINT      NOT NULL,
+  login VARCHAR(45) NOT NULL,
+  role  VARCHAR(45) NOT NULL,
+  UNIQUE (login, role)
+);
+
+ALTER TABLE user_roles ADD FOREIGN KEY(login) REFERENCES users(login);
