e95aae1a96ff099d6c9d4029cb9e6efe9b6a2772
dzinushi <dzinushi.kun@gmail.com>
2017-06-09 17:20:58 +0300

    Remove @ComponentScan

--- src/main/java/com/mev/films/config/AppConfig.java
+++ src/main/java/com/mev/films/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.mev.films.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -11,7 +10,6 @@ import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan({"com.mev.films.*"})
 @Import({SecurityConfig.class})
 public class AppConfig {
 
