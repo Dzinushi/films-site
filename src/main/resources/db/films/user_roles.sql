92c575ecc621fe7936ffe2861c6a9db88d40a0f8
dzinushi <dzinushi.kun@gmail.com>
2017-06-09 15:47:46 +0300

    Append flag 'AUTOINCREMENT' for 'id' in all tables

--- src/main/resources/db/films/user_roles.sql
+++ src/main/resources/db/films/user_roles.sql
@@ -1,6 +1,6 @@
 CREATE SEQUENCE user_roles_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
 CREATE TABLE user_roles (
-  id    BIGINT      NOT NULL,
+  id    BIGINT NOT NULL DEFAULT nextval('user_roles_id_seq'::regclass),
   login VARCHAR(45) NOT NULL,
   role  VARCHAR(45) NOT NULL,
   UNIQUE (login, role)
