be589643b151fe1b2ec90faf09f6c4e95c62c12b
dzinushi <dzinushi.kun@gmail.com>
2017-06-09 12:37:21 +0300

    Create BasketMapper

---
+++ src/main/java/com/mev/films/mappers/interfaces/BasketMapper.java
@@ -0,0 +1,13 @@
+package com.mev.films.mappers.interfaces;
+
+import com.mev.films.model.BasketDTO;
+
+import java.util.List;
+
+public interface BasketMapper {
+    List<BasketDTO> selectBaskets();
+    List<BasketDTO> selectBasketByUserName(String userName);
+    Long insertBasket(BasketDTO basketDTO);
+    Long updateBasket(BasketDTO basketDTO);
+    Long deleteBasketByUserName(String userName);
+}
