10adc69f40aa89bd67d883c25fcd7db1a27f64e8
dzinushi <dzinushi.kun@gmail.com>
2017-06-09 17:18:50 +0300

    Added configs for Spring-security

---
+++ src/main/java/com/mev/films/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.mev.films.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+}
