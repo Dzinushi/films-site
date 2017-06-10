ae4d3dc66b81b58ee98a0203a9efc6c9acd68c88
dzinushi <dzinushi.kun@gmail.com>
2017-06-10 17:16:37 +0300

    Added bean UserRoleMapper. Update addUser, updateUser, deleteUser: added beans with userRole for this operation. Added addUserRole, updateUserRole, deleteUserRole.

--- src/main/java/com/mev/films/service/implement/UserServiceImpl.java
+++ src/main/java/com/mev/films/service/implement/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.mev.films.service.implement;
 
 import com.mev.films.mappers.interfaces.UserMapper;
+import com.mev.films.mappers.interfaces.UserRoleMapper;
 import com.mev.films.model.UserDTO;
+import com.mev.films.model.UserRoleDTO;
 import com.mev.films.service.interfaces.UserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,7 +20,7 @@ public class UserServiceImpl implements UserService{
     private static final Logger LOG = LogManager.getLogger();
 
     @Autowired private UserMapper userMapper;
-
+    @Autowired private UserRoleMapper userRoleMapper;
 
     @Override
     public List<UserDTO> getAllUsers() {
@@ -27,24 +29,44 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public Long addUser(UserDTO userDTO) {
-        LOG.debug("addUser: id = {}, login = {}, password = {}",
-                userDTO.getId(), userDTO.getLogin(), userDTO.getPassword());
+    public Long addUser(UserDTO userDTO, UserRoleDTO userRoleDTO) {
+        LOG.debug("addUser: id = {}, login = {}, password = {}, role = {}",
+                userDTO.getId(), userDTO.getLogin(), userDTO.getPassword(), userRoleDTO.getRole());
         return userMapper.insertUser(userDTO);
     }
 
     @Override
-    public Long updateUser(UserDTO userDTO) {
-        LOG.debug("updateUser: id = {}, login = {}, password = {}",
-                userDTO.getId(), userDTO.getLogin(), userDTO.getPassword());
+    public Long addUserRole(UserRoleDTO userRoleDTO) {
+        LOG.debug("addRole: id = {}, login = {}, role = {}",
+                userRoleDTO.getId(), userRoleDTO.getLogin(), userRoleDTO.getRole());
+        return userRoleMapper.insertUserRole(userRoleDTO);
+    }
+
+    @Override
+    public Long updateUser(UserDTO userDTO, UserRoleDTO userRoleDTO) {
+        LOG.debug("updateUser: id = {}, login = {}, password = {}, role = {}",
+                userDTO.getId(), userDTO.getLogin(), userDTO.getPassword(), userRoleDTO.getRole());
         return userMapper.updateUser(userDTO);
     }
 
     @Override
+    public Long updateUserRole(UserRoleDTO userRoleDTO) {
+        LOG.debug("updateRole: id = {}, login = {}, role = {}",
+                userRoleDTO.getId(), userRoleDTO.getLogin(), userRoleDTO.getRole());
+        return userRoleMapper.updateUserRole(userRoleDTO);
+    }
+
+    @Override
     public Long deleteUser(String login) {
-        UserDTO userDTO = userMapper.selectUserByName(login);
-        LOG.debug("deleteUser: id = {}, login = {}, password = {}",
-                userDTO.getId(), userDTO.getLogin(), userDTO.getPassword());
+        LOG.debug("deleteUser: login = {}",
+                login);
         return userMapper.deleteUserByLogin(login);
+    }
+
+    @Override
+    public Long deleteUserRole(String login, String role) {
+        LOG.debug("deleteUserRole: login = {}m role = {}",
+                login, role);
+        return userRoleMapper.deleteUserRoleByLoginRole(login, role);
     }
 }
--- src/main/java/com/mev/films/service/interfaces/UserService.java
+++ src/main/java/com/mev/films/service/interfaces/UserService.java
@@ -1,12 +1,16 @@
 package com.mev.films.service.interfaces;
 
 import com.mev.films.model.UserDTO;
+import com.mev.films.model.UserRoleDTO;
 
 import java.util.List;
 
 public interface UserService {
     List<UserDTO> getAllUsers();
-    Long addUser(UserDTO userDTO);
-    Long updateUser(UserDTO userDTO);
+    Long addUser(UserDTO userDTO, UserRoleDTO userRoleDTO);
+    Long addUserRole(UserRoleDTO userRoleDTO);
+    Long updateUser(UserDTO userDTO, UserRoleDTO userRoleDTO);
+    Long updateUserRole(UserRoleDTO userRoleDTO);
     Long deleteUser(String login);
+    Long deleteUserRole(String login, String role);
 }
