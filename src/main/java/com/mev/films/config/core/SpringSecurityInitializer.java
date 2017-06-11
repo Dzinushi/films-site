c4d28bb31f6a32a75643d8a6e8dc1296fcb5f548
dzinushi <dzinushi.kun@gmail.com>
2017-06-11 22:21:03 +0300

    Some changes

--- src/main/java/com/mev/films/config/core/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.mev.films.config.core;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer{
-}
