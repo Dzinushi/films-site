9c38132c4793cf1ff0565ba53f9d1e24e80de146
dzinushi <dzinushi.kun@gmail.com>
2017-06-29 21:33:56 +0300

    Some changes

--- src/main/java/com/mev/films/service/implement/FilmServiceImpl.java
+++ src/main/java/com/mev/films/service/implement/FilmServiceImpl.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -20,6 +21,7 @@ public class FilmServiceImpl implements FilmService{
     @Autowired private FilmMapper filmMapper;
 
     @Override
+    @RequestMapping(value = "getAllFilms")
     public List<FilmDTO> getAllFilms() {
         LOG.debug("getAllFilms");
 
